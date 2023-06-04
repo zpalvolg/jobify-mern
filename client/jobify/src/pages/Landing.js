a75ae85c412ad9fdd3224aa4ff023e02bfb6f0b0
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-04 18:34:09 +0200

    added Landing and installed styled-components

---
+++ client/jobify/src/pages/Landing.js
@@ -0,0 +1,26 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+
+function Landing() {
+    return (
+      <Wrapper>
+        <nav>
+            <img src={logo} alt="jobify" className="logo" />
+        </nav>
+        <div className="container page">
+            <div className="info">
+                <h1>job <span>tracking</span> app</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu diam euismod, viverra ante sed, rhoncus dolor. Vestibulum nisl tortor, tincidunt nec eros vel, iaculis tempor mi. Nam at diam non sapien posuere tristique ut quis mi. Mauris id sem vel diam commodo vehicula. Nulla suscipit, nisl eget gravida cursus, leo dui commodo risus, in pulvinar est urna nec tortor. Cras euismod turpis sapien, quis faucibus sem molestie viverra. Nunc ut turpis sit amet massa ultricies ultricies vel eu est. 
+                </p>
+                <button className="btn btn-hero">Login/Register</button>
+            </div>
+            <img src={main} alt="job hunt" className="img main-img" />
+        </div>
+      </Wrapper>
+    );
+}
+  
+export default Landing;
+  
