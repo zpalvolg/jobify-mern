f8be3df86d74fa33d5d40678a6d1f49618c5c92d
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-05 10:41:24 +0200

    added Routes

--- client/jobify/src/pages/Landing.js
+++ client/jobify/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import {Logo} from '../components'
+import { Link } from 'react-router-dom';
 
 function Landing() {
     return (
@@ -14,7 +15,7 @@ function Landing() {
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu diam euismod, viverra ante sed, rhoncus dolor. Vestibulum nisl tortor, tincidunt nec eros vel, iaculis tempor mi. Nam at diam non sapien posuere tristique ut quis mi. Mauris id sem vel diam commodo vehicula. Nulla suscipit, nisl eget gravida cursus, leo dui commodo risus, in pulvinar est urna nec tortor. Cras euismod turpis sapien, quis faucibus sem molestie viverra. Nunc ut turpis sit amet massa ultricies ultricies vel eu est. 
                 </p>
-                <button className="btn btn-hero">Login/Register</button>
+                <Link to="/register" className="btn btn-hero">Login/Register</Link>
             </div>
             <img src={main} alt="job hunt" className="img main-img" />
         </div>
---
+++ client/jobify/src/pages/index.js
@@ -0,0 +1,11 @@
+import Landing from "./Landing";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
+import Error from "./Error";
+
+export {
+    Landing
+    , Register
+    , Dashboard
+    , Error
+}
