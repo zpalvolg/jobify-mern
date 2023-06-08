53461a9918474418053d805dc7678de479ff163a
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-08 12:05:47 +0200

    added checking logic in errorHandlerMiddleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,8 +2,27 @@ import {StatusCodes} from 'http-status-codes'
 
 const errorHandlerMiddleware = (err,req,res,next) => {
     console.log(err)
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        msg: err
+
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR
+        ,msg: 'Something went wrong, try again later.'
+    }
+
+    //DB errors: checking requiered fields and validation like correct email address
+    if (err.name === 'ValidationError'){
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = err.message
+    }
+
+    //DB errors: checking unique fileds
+    if(err.code && err.code === 11000){
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
+    }
+
+
+    res.status(defaultError.statusCode).json({
+        msg: defaultError.msg //err
     })
 }
 
