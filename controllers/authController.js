2a46f4ff0cd36537f74f251604eb7518c9978c40
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-09 13:59:31 +0200

    added JSON Web Token to User Schema

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,19 @@ import {StatusCodes} from 'http-status-codes'
 const register = async (req,res) => {
     //using express-async-errors we can skip try/catch and the error will be passed to errorHandlerMiddleware
     const user = await User.create(req.body)
-    res.status(StatusCodes.CREATED).json({user})
+
+    const token = user.createJWT()
+
+    res.status(StatusCodes.CREATED).json({
+        user:{
+            email: user.email
+            ,lastName: user.lastName
+            ,location: user.location
+            ,name: user.name
+        }
+        , token
+        , location: user.location
+    })
 }
 
 const login = async (req,res) => {
