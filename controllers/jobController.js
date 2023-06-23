58a123172d16b2148c96057c2f26ba0f8d6a47c9
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-23 16:40:58 +0200

    added Job model and createJob controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,22 @@
-const createJob = async (req,res) => {
-    res.send("create job")
-}
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+
+const createJob = async (req, res) => {
+    const { position, company } = req.body;
+  
+    if (!position || !company) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            msg: "Provide all values!"
+        })
+    } else {
+        req.body.createdBy = req.user.userId;
+  
+        const job = await Job.create(req.body);
+        
+        res.status(StatusCodes.CREATED).json({ job });
+    }
+  
+  };
 
 const deleteJob = async (req,res) => {
     res.send("delete job")
