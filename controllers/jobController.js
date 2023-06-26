5fbb68f21e938e5a4473fbf7457dd283a08c8060
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-26 13:06:28 +0200

    added logic to getAllJobs in controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -22,9 +22,13 @@ const deleteJob = async (req,res) => {
     res.send("delete job")
 }
 
-const getAllJobs = async (req,res) => {
-    res.send("get all jobs")
-}
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({ createdBy: req.user.userId });
+  
+    res
+      .status(StatusCodes.OK)
+      .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+};
 
 const updateJob = async (req,res) => {
     res.send("updateJob")
