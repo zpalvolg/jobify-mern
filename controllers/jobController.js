05245ea0ca50aca581b60cad8d15f3942fe876e9
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-06-30 14:42:31 +0200

    added logic to deleteJob and updateJob

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -18,9 +18,22 @@ const createJob = async (req, res) => {
   
   };
 
-const deleteJob = async (req,res) => {
-    res.send("delete job")
-}
+const deleteJob = async (req, res) => {
+    const { id: jobId } = req.params;
+  
+    const job = await Job.findOne({ _id: jobId });
+  
+    if (!job) {
+        res.status(StatusCodes.NOT_FOUND).json({
+            msg: "No job with this id!"
+        })
+    }else {
+  
+    await job.deleteOne();
+    
+    res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
+    }
+};
 
 const getAllJobs = async (req, res) => {
     const jobs = await Job.find({ createdBy: req.user.userId });
@@ -30,9 +43,35 @@ const getAllJobs = async (req, res) => {
       .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 
-const updateJob = async (req,res) => {
-    res.send("updateJob")
-}
+const updateJob = async (req, res) => {
+    const { id: jobId } = req.params;
+  
+    const { company, position } = req.body;
+  
+    if (!company || !position) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            msg: "Provide all values!"
+        })
+    }else {
+        const job = await Job.findOne({ _id: jobId });
+    
+        if (!job) {
+            res.status(StatusCodes.NOT_FOUND).json({
+                msg: "No job with this id!"
+            })
+        }else {
+        
+            // check permissions
+        
+            const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+            new: true,
+            runValidators: true,
+            });
+
+            res.status(StatusCodes.OK).json({ updatedJob });
+        }
+    }
+};
 
 const showStats = async (req,res) => {
     res.send("show stats")
