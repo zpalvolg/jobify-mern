b8b4238b3bfc51819f9f1ce301f149522a51e3a2
Zoltan Palvolgyi <palvolgyi.zoltan91@gmail.com>
2023-07-10 11:10:35 +0200

    modifed getAllJobs for pagination on server

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -38,42 +38,47 @@ const deleteJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-    const { search, status, jobType, sort } = req.query;
-  
-    const queryObject = {
-      createdBy: req.user.userId,
-    };
-  
-    if (status !== 'all') {
-      queryObject.status = status;
-    }
-    if (jobType !== 'all') {
-      queryObject.jobType = jobType;
-    }
-    if (search) {
-      queryObject.position = { $regex: search, $options: 'i' };
-    }
-    // NO AWAIT
-    let result = Job.find(queryObject);
-  
-    // chain sort conditions
-    if (sort === 'latest') {
-      result = result.sort('-createdAt');
-    }
-    if (sort === 'oldest') {
-      result = result.sort('createdAt');
-    }
-    if (sort === 'a-z') {
-      result = result.sort('position');
-    }
-    if (sort === 'z-a') {
-      result = result.sort('-position');
-    }
-    const jobs = await result;
-  
-    res
-      .status(StatusCodes.OK)
-      .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const { search, status, jobType, sort } = req.query;
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
+  if (status !== 'all') {
+    queryObject.status = status;
+  }
+  if (jobType !== 'all') {
+    queryObject.jobType = jobType;
+  }
+  let result = Job.find(queryObject);
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position');
+  }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
